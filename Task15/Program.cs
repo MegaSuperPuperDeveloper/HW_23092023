253d4c624c9fb1b90ec33bd8c7bef33721419c47
Mega Krutoi <krutoi.chuvak1918@gmail.com>
2023-09-24 21:12:23 +0300

    V.1

---
+++ Task15/Program.cs
@@ -0,0 +1,4 @@
+﻿string[] week = {"нет", "нет", "нет", "нет", "нет", "да", "да"};
+Console.Write("Введите число от 1 до 7: ");
+short a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"{a} = {week[a - 1]}");
