a7015762a2f7b26dbd9f0f5a4304f44b5cb42bc3
Mega Krutoi <krutoi.chuvak1918@gmail.com>
2023-09-24 21:12:11 +0300

    V.1

---
+++ Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+string digit = Console.ReadLine();
+int length = digit.Length;
+if(length < 3) {
+    Console.WriteLine($"{digit} -> третьей цифры нет");
+} else {
+    char ThirdDigit = digit[2];
+    Console.WriteLine(ThirdDigit);
+}
